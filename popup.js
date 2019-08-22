07992eb208dd0c560e2e5a09a09ab6213451ed9f
Tim Darrow <timothy.honor7@gmail.com>
2019-08-22 11:06:06 -0700

    Creation of nav object in DOM
    Nav is created and destroyed when a new element is clicked on

--- popup.js
+++ popup.js
@@ -1,40 +1,90 @@
+// declare default variables
 let el = null, 
     parentEl = null,
-    tree = [];
+    tree = []
 
+// Declare function that builds a new node tree based on what you clicked on.
 function findNodeTree(event) { 
-    if (tree.length != 0)
+    // if a tree already exists due to a previous click, call the clearNodeTree function
+    if (tree.length > 0) {
         clearNodeTree();
+    }
+
+    // set variables to elements that you clicked on
     el = event.target;
     parentEl = el.parentNode;
+
+    // Push element into tree array
+    setMultipleAttributes(el, { 'data-copy': 'true', 'data-cNode': tree.length });
     tree.push(el);
-    while(parentEl) {
+    // el.setAttribute('data-copy', 'true');
+    // el.setAttribute('data-cNode', tree.length);
+    while(parentEl && parentEl.nodeName != '#document') {
+        parentEl.setAttribute('data-cNode', tree.length);
         tree.push(parentEl);
-        parentEl = parentEl.parentNode
+        parentEl = parentEl.parentNode;
     }
+
+    // log the tree temporarily so we can see what we are looking at
     console.log(tree);
-    for (let i = 0; i < tree.length - 1; i++) {
-        tree[i].setAttribute('data-cNode', i);
-        if (i == 0) 
-            tree[i].setAttribute('data-copy', 'true');
-    }
+
+    // create visual representation of tree
+    createRepresentationOfTree();
+
+    // call text highlight function
     triggerHighlightFunction();
 }
 
+// Placeholder function used to call highlighter div
 function triggerHighlightFunction() {
     console.log('placeholder');
 }
 
+// clear all data-cNode and data-copy attributes out of existing tree
 function clearNodeTree() {
-    for (let i = 0; i < tree.length - 1; i++) {
+    let nav = document.querySelector('[data-nav]');
+    nav.remove();
+    for (let i = 0; i < tree.length; i++) {
         tree[i].removeAttribute('data-cNode');
-        if (i == 0) 
+        if (tree[i].hasAttribute('data-copy')) 
             tree[i].removeAttribute('data-copy');
     }
+    // destroy the current tree
     tree = [];
 }
 
+// function that builds navigable representation of tree
+function createRepresentationOfTree() {
+    let nav = document.createElement('nav');
+    nav.setAttribute('data-nav', '');
+    for (let i = 0; i < tree.length; i++) {
+        let link = document.createElement('a');
+        setMultipleAttributes(link, { 'data-cNode': tree[i].getAttribute('data-cNode'), 'href': 'javascript:void(0)' });
+        link.innerText = tree[i].nodeName.toString().toLowerCase();
+        console.log(link);
+        nav.prepend(link);
+        if(i < tree.length - 1) {
+            let separator = document.createElement('span');
+            separator.className += 'separator';
+            separator.innerText = '>';
+            nav.prepend(separator)
+        }
+    }
+    document.body.appendChild(nav);
+}
+
+
+// helper function to set multiple attributes at a time
+// note that the function is looking for both an element and a map to be passed as variables
+function setMultipleAttributes(el, attrMap) {
+    for(let key in attrMap) {
+        el.setAttribute(key, attrMap[key]);
+    }
+}
+
+// listen for click events in the window. On click, call findNodeTree function
 window.addEventListener('click', () => {
+
     event.preventDefault();
     findNodeTree(event);
 });
