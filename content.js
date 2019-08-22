7ad0fb774d6cb8e04e5f644abae6ca14e9959b21
Marcus LeGault <marcus.legault@scorpion.co>
2019-08-22 15:40:31 -0700

    highlighter complete

--- content.js
+++
@@ -1,5 +0,0 @@
-var body = document.getElementsByTagName('body');
-var elemDiv = document.createElement('div');
-elemDiv.innerText = "hey gurl"
-
-document.body.appendChild(elemDiv);
--- highlighter.js
+++ highlighter.js
@@ -1,1 +1,23 @@
-//TODO
+var body = document.getElementsByTagName('body');
+var highlight = document.createElement('div');
+
+document.addEventListener('click', function (e) {
+    var item = e.target;
+    getID(item);
+});
+
+function setAttributes(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
+function getID(el) {
+    var styleString = "position: absolute !important; visibility: visible !important; opacity: 1 !important; height: " + el.offsetHeight + "px; width: " + el.offsetWidth + "px; left: " + el.offsetLeft + "px; top: " + el.offsetTop + "px";
+    setAttributes(highlight, {
+        "class": "highlighter",
+        "style": styleString
+    });
+}
+
+document.body.appendChild(highlight);
