014d58c3aac02e371d08ef12a0c27317a955c824
Tim Darrow <timothy.honor7@gmail.com>
2019-08-23 12:04:07 -0700

    added secondary copy button

--- highlighter.js
+++ highlighter.js
@@ -1,8 +1,10 @@
 let highlight = document.createElement('div'),
     hoverHighlight = document.createElement('div')
-    copyButton = document.createElement('a');
+    copyButton1 = document.createElement('a');
+    copyButton2 = document.createElement('a');
 
-copyButton.innerText = 'Copy Contents';
+copyButton1.innerText = 'Copy Content';
+copyButton2.innerText = 'Copy Content';
 
 function setAttributes(el, attrs) {
     for (var key in attrs) {
@@ -15,17 +17,25 @@ function getID(el) {
         elemRect = el.getBoundingClientRect(),
         top = (elemRect.top - bodyRect.top),
         left = (elemRect.left - bodyRect.left);
+        right = (elemRect.right - bodyRect.right);
     var styleString = 'visibility: visible !important; opacity: 1 !important; height: ' + el.offsetHeight + 'px; width: ' + el.offsetWidth + 'px; left: ' + left + 'px; top: ' + top + 'px';
     setAttributes(highlight, {
         'class': 'highlighter',
         'style': styleString,
         'data-cbspecial': ''
     });
-    setAttributes(copyButton, {
+    setAttributes(copyButton1, {
         'class': 'copy-button',
         'data-cbcopy': 'single',
         'data-cbspecial': '',
         'style': 'visibility: visible !important; opacity: 1 !important; top: ' + calculateButtonPosition(top) + 'px; left: ' + left + 'px'
+    });
+
+    setAttributes(copyButton2, {
+        'class': 'copy-button',
+        'data-cbcopy': 'single',
+        'data-cbspecial': '',
+        'style': 'visibility: visible !important; opacity: 1 !important; top: ' + (top + el.offsetHeight + 10) + 'px; left: ' + (left + el.offsetWidth - copyButton2.offsetWidth) + 'px'
     });
 
     checkScrollPositon(top, bodyRect.top * -1);
@@ -46,8 +56,16 @@ function moveHoverHighlight(el) {
 }
 
 function calculateButtonPosition(top) {
-    let topPos = top - (copyButton.offsetHeight) - 5;
-    // console.log(copyButton.offsetHeight);
+    let topPos = top - (copyButton1.offsetHeight) - 5;
+    // console.log(copyButton1.offsetHeight);
+    // console.log(top);
+    // console.log(topPos);
+    return (topPos < 40) ? top : topPos;
+}
+
+function calculateButtonPosition2(top) {
+    let topPos = top - (copyButton1.offsetHeight) - 5;
+    // console.log(copyButton1.offsetHeight);
     // console.log(top);
     // console.log(topPos);
     return (topPos < 40) ? top : topPos;
@@ -76,4 +94,5 @@ function checkScrollPositon(top, bodyTop) {
 }
 
 document.body.appendChild(highlight);
-document.body.appendChild(copyButton);
+document.body.appendChild(copyButton1);
+document.body.appendChild(copyButton2);
